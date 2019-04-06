68edd7aff4981da0be78ac7f53db3a5371a9e108
visualstudio <visualstudio@122-PC29>
2019-04-05 22:06:21 -0400

    SECTION E

--- C0730065 Assignment4/Program.cs
+++ C0730065 Assignment4/Program.cs
@@ -18,216 +18,127 @@ namespace C0730065_Assignment4
         static void Main(string[] args)
         {
             Program p = new Program();
-
             p.Beowulf = new ArrayList();
-
-            p.Wordskipper();
-
+            p.Letters();
             Console.ReadLine();
-
-
-
         }
-
         public void Run()
-
         {
-
             this.ReadTextFiles();
-
         }
-
-
-
         public void ReadTextFiles()
-
         {
-
-
-
             // Read file using StreamReader. Read file line by line
-
-            using (StreamReader file = new StreamReader("U:/Users/730065/Assignment4/Beowulf.txt"))
-
+            using (StreamReader file = new StreamReader("U:/Users/730065/Assignment/Beowulf.txt"))
             {
-
                 int counter = 0;
-
                 string ln;
-
-
-
                 while ((ln = file.ReadLine()) != null)
-
                 {
-
                     Console.WriteLine(ln);
-
                     Beowulf.Add(ln);
-
                 }
-
                 file.Close();
-
                 Console.WriteLine($"File has {counter} lines.");
-
             }
-
         }
-
         public int FindNumberOfBlankSpaces(string line)
-
         {
-
             int countletters = 0;
-
             int countSpaces = 0;
-
             {
-
                 foreach (char c in line)
-
                 {
-
                     if (char.IsLetter(c)) { countletters++; }
-
                     if (char.IsWhiteSpace(c)) { countSpaces++; }
-
                 }
-
                 return countSpaces;
-
             }
-
         }
-
         public void CountLinesReader()
-
         {
-
             long lineCounter = 0;
-
-            using (StreamReader fil = new StreamReader("U:/Users/730065/Assignment4/Beowulf.txt"))
-
+            using (StreamReader fil = new StreamReader("U:/Users/730065/Assignment/Beowulf.txt"))
             {
-
                 while (fil.ReadLine() != null)
-
                 {
-
                     lineCounter++;
-
                 }
-
                 Console.WriteLine(lineCounter);
-
             }
-
         }
-
         public void WordCounter()
-
         {
-
-
-
-            StreamReader reader = new StreamReader("U:/Users/730065/Assignment4/Beowulf.txt");
-
+            StreamReader reader = new StreamReader("U:/Users/730065/Assignment/Beowulf.txt");
             string script = reader.ReadToEnd();
-
-
-
             var text = script.Trim();
-
             int wordCount = 0, index = 0;
-
-
-
             while (index < text.Length)
-
             {
-
                 // check if current char is part of a word
-
                 while (index < text.Length && !char.IsWhiteSpace(text[index]))
-
                     index++;
-
-
-
                 wordCount++;
-
-
-
                 // skip whitespace until next word
-
                 while (index < text.Length && char.IsWhiteSpace(text[index]))
-
                     index++;
-
             }
-
-
-
             Console.WriteLine(wordCount);
-
-
-
-
-
         }
-
         public void Wordfinder()
-
         {
-
             int f = 0;
-
-            foreach (string line in File.ReadAllLines("U:/Users/730065/Assignment4/Beowulf.txt"))
-
+            foreach (var line in File.ReadAllLines("U:/Users/730065/Assignment/Beowulf.txt"))
             {
-
+                f++;
                 if (line.Contains("sea") && line.Contains("fare"))
-
                 {
-
-                    f++;
-
+                    Console.WriteLine(f);
                 }
-
-
-
             }
-
-            Console.WriteLine(f);
-
         }
         public void Wordskipper()
         {
             int f = 0;
-            int m = 0;
-            int n = 0;
-            foreach (var line in File.ReadAllLines("U:/ Users / 730065 / Assignment4/Beowulf.txt"))
+            foreach (var line in File.ReadAllLines("U:/Users/730065/Assignment/Beowulf.txt"))
             {
-                if (line.Contains("fare"))
+                f++;
+                if (line.Contains("fare") && (!line.Contains("war")))
                 {
-                    f++;
+                    Console.WriteLine(f);
                 }
             }
-            foreach (string line in File.ReadAllLines("U:/ Users / 730065 / Assignment4/Beowulf.txt"))
+        }
+        public void Letters()
+        {
+            StreamReader reader = new StreamReader("U:/Users/730065/Assignment/Beowulf.txt");
+            string script = reader.ReadToEnd();
+            //find number of letters
+            int numberOfLetters = 0;
+            foreach (char letter in script)
             {
-                if (line.Contains("war") && line.Contains("fare"))
-                {
-                    m++;
-                }
+                numberOfLetters++;
             }
-            n = f - m;
-            Console.WriteLine(n);
+            var text = script.Trim();
+            int wordCount = 0, index = 0;
+            //find number of words
+            while (index < text.Length)
+            {
+                // check if current char is part of a word
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+                wordCount++;
+                // skip whitespace until next word
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+            //find average
+            float avrg = numberOfLetters / wordCount;
+            Console.WriteLine(avrg);
         }
     }
 }
 
-    
 
 
 
